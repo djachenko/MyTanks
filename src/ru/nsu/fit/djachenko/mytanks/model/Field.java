3fdf18a69a3e82862d3141fd267761f329506d41
djachenko <i.s.djachenko@gmail.com>
2013-08-17 04:34:45 +0700

    Structure change

--- src/ru/nsu/fit/djachenko/mytanks/model/Field.java
+++ src/ru/nsu/fit/djachenko/mytanks/model/Field.java
@@ -1,9 +1,8 @@
-package ru.nsu.fit.djachenko.mytanks;
+package ru.nsu.fit.djachenko.mytanks.model;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
 public class Field
 {
--- src/ru/nsu/fit/djachenko/mytanks/model/Tank.java
+++ src/ru/nsu/fit/djachenko/mytanks/model/Tank.java
@@ -1,4 +1,4 @@
-package ru.nsu.fit.djachenko.mytanks;
+package ru.nsu.fit.djachenko.mytanks.model;
 
 public class Tank
 {
