35970873d99b81f0d8a84f76dde01decb1f3e9dc
djachenko <i.s.djachenko>
2013-08-26 03:33:37 +0700

    Class for controlling keyboard events.

---
+++ src/ru/nsu/fit/djachenko/mytanks/view/Controller.java
@@ -0,0 +1,63 @@
+package ru.nsu.fit.djachenko.mytanks.view;
+
+import ru.nsu.fit.djachenko.mytanks.model.Direction;
+import ru.nsu.fit.djachenko.mytanks.model.Level;
+import ru.nsu.fit.djachenko.mytanks.model.UnexpectedSituationException;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Controller extends KeyAdapter
+{
+	private Level level;
+
+	Controller(Level level)
+	{
+		this.level = level;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		int key = e.getKeyCode();
+
+		try
+		{
+			switch (key)
+			{
+				case KeyEvent.VK_RIGHT:
+				case KeyEvent.VK_D:
+					level.moveTank(Direction.RIGHT);
+					break;
+
+				case KeyEvent.VK_UP:
+				case KeyEvent.VK_W:
+					level.moveTank(Direction.UP);
+					break;
+
+				case KeyEvent.VK_LEFT:
+				case KeyEvent.VK_A:
+					level.moveTank(Direction.LEFT);
+					break;
+
+				case KeyEvent.VK_DOWN:
+				case KeyEvent.VK_S:
+					level.moveTank(Direction.DOWN);
+					break;
+
+				case KeyEvent.VK_SPACE:
+					level.shoot();
+					break;
+
+				default:
+					break;
+			}
+		}
+		catch (UnexpectedSituationException e1)
+		{
+			e1.printStackTrace();
+		}
+
+		//level.print();
+	}
+}
