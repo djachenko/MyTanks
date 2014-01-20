256fcafbcce034dc2222f38f1bab971e5fba095b
djachenko <i.s.djachenko@gmail.com>
2014-01-20 09:00:52 +0700

    Bullet speed changed.

--- src/ru/nsu/fit/djachenko/mytanks/model/activities/MoveBulletTask.java
+++ src/ru/nsu/fit/djachenko/mytanks/model/activities/MoveBulletTask.java
@@ -6,7 +6,7 @@ public class MoveBulletTask implements Task
 {
 	private final Bullet bullet;
 
-	private final static int SCIPCOUNT = 5;
+	private final static int SCIPCOUNT = 2;
 
 	public MoveBulletTask(Bullet bullet)
 	{
